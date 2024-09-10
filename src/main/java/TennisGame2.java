728c1d4a287782e823fcb1003061ad5948716195
Hamza Iddaoui <hiddaoui@sqli.com>
2024-09-10 10:05:38 +0100

    some changes

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -1,6 +1,13 @@
 import tennis2enums.GameState;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TennisGame2 implements TennisGame {
+    public static final String LOVE = "Love";
+    public static final String FIFTEEN = "Fifteen";
+    public static final String THIRTY = "Thirty";
+    public static final String FORTY = "Forty";
     public int P1point = 0;
     public int P2point = 0;
 
@@ -8,74 +15,68 @@ public class TennisGame2 implements TennisGame {
     public String P2res = "";
     private String player1Name;
     private String player2Name;
+    private Map<String, Integer> playersScore = new HashMap<>();
 
     public TennisGame2(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
+        playersScore.putAll(Map.of(player1Name, 0, player2Name, 0));
     }
 
     public String getScore() {
         String score = "";
-        switch (GameState.forScores(P1point, P2point)){
-            case PLAYER1_WITH_HIGHER_SCORE -> {
-                if (P2point == 0) {
-                    P1res = titleByScore(P1point);
-                    P2res = "Love";
-                }
-                if (P1point < 4) {
-                    P1res = titleByScore(P1point);
-                    P2res = titleByScore(P2point);
-                    score = P1res + "-" + P2res;
-                }
-                if (P1point >= 4) {
-                    score = "Advantage player1";
-                    if (P2point >= 0) {
-                        if ((P1point - P2point) >= 2) {
-                            score = "Win for player1";
-                        }
-                    }
-                }
-            }
-
-            case PLAYER2_WITH_HIGHER_SCORE -> {
-                if (P1point == 0) {
-                    P2res = titleByScore(P2point);
-                    P1res = "Love";
-                }
-                if (P2point < 4) {
-                    P2res = titleByScore(P2point);
-                    P1res = titleByScore(P1point);
-                    score = P1res + "-" + P2res;
-                }
-                if (P2point >= 4) {
-                    score = "Advantage player2";
-                    if (P1point >= 0) {
-                        if ((P2point - P1point) >= 2) {
-                            score = "Win for player2";
-                        }
-                    }
-                }
-            }
-
-            case PLAYERS_WITH_SAME_SCORE -> {
-                if (P1point < 4) {
-                    score = titleByScore(P1point);
-                    score += "-All";
-                }
-                if (P1point >= 3) {
-                    score = "Deuce";
-                }
-            }
+        switch (GameState.forScores(playersScore.get(player1Name), playersScore.get(player2Name))) {
+            case PLAYER1_WITH_HIGHER_SCORE -> score = buildScoreWhenPlayer1HasHigherScore();
+            case PLAYER2_WITH_HIGHER_SCORE -> score = buildScoreWhenPlayer2HasHigherScore();
+            case PLAYERS_WITH_SAME_SCORE -> score = buildDrawScore();
         }
         return score;
     }
 
+    private String buildScoreWhenPlayer2HasHigherScore() {
+        P2res = titleByScore(playersScore.get(player2Name));
+        P1res = titleByScore(playersScore.get(player1Name));
+        String result = "";
+        if (playersScore.get(player2Name) < 4) {
+            result = P1res + "-" + P2res;
+        }
+        if (playersScore.get(player2Name) >= 4) {
+            result = (playersScore.get(player2Name) - playersScore.get(player1Name)) >= 2 ? "Win for player2" : "Advantage player2";
+        }
+        return result;
+    }
+
+    private String buildScoreWhenPlayer1HasHigherScore() {
+        P1res = titleByScore(playersScore.get(player1Name));
+        P2res = titleByScore(playersScore.get(player2Name));
+        String result = "";
+        if (playersScore.get(player1Name) < 4) {
+            result = P1res + "-" + P2res;
+        }
+        if (playersScore.get(player1Name) >= 4) {
+            result = (playersScore.get(player1Name) - playersScore.get(player2Name)) >= 2 ? "Win for player1" : "Advantage player1";
+        }
+        return result;
+    }
+
+    private String buildDrawScore() {
+        String result;
+        result = titleByScore(playersScore.get(player1Name));
+        if (playersScore.get(player1Name) < 4) {
+            result += "-All";
+        }
+        if (playersScore.get(player1Name) >= 3) {
+            result = "Deuce";
+        }
+        return result;
+    }
+
     private String titleByScore(int score) {
         return switch (score) {
-            case 0 -> "Love";
-            case 1 -> "Fifteen";
-            case 2 -> "Thirty";
-            case 3 -> "Forty";
+            case 0 -> LOVE;
+            case 1 -> FIFTEEN;
+            case 2 -> THIRTY;
+            case 3 -> FORTY;
             default -> "";
         };
     }
@@ -97,17 +98,14 @@ public class TennisGame2 implements TennisGame {
     }
 
     public void P1Score() {
-        P1point++;
+        playersScore.put(player1Name,playersScore.get(player1Name) + 1);
     }
 
     public void P2Score() {
-        P2point++;
+        playersScore.put(player2Name,playersScore.get(player2Name) + 1);
     }
 
     public void wonPoint(String player) {
-        if (player == "player1")
-            P1Score();
-        else
-            P2Score();
+        playersScore.put(player, playersScore.get(player) + 1);
     }
 }
